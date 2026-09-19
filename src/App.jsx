58c059592e7e691015f4291749cfa20759e51c04
silc-o <bhussienaldondie@gmail.com>
2026-09-19 16:31:42 +0800

    feat: implement card fetching logic and initial state setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,20 @@
-export default function App() { return <h1>Memory Card</h1>; }
+import { useEffect, useState } from "react";
+
+export default function App() {
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    const fetchCards = async () => {
+      const response = await fetch(
+        "https://pokeapi.co/api/v2/pokemon?limit=12",
+      );
+      const data = await response.json();
+      setCards(data.results);
+      console.log(data);
+    };
+
+    fetchCards();
+  }, []);
+
+  return <div>{/* render cards here */}</div>;
+}
